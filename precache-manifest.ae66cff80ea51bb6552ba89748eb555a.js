9044308484bb61b9c2a0af92de04cbb79f2271d1
Connie Tran <connietrandev@gmail.com>
2020-07-15 10:15:00 -0400

    Updates

---
+++ precache-manifest.ae66cff80ea51bb6552ba89748eb555a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f2d1f85689b753e90501251ac26e50",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "a7940a71626437234098",
+    "url": "/react-employee-directory/static/css/main.536d0e21.chunk.css"
+  },
+  {
+    "revision": "5dddd572a61f3339c038",
+    "url": "/react-employee-directory/static/js/2.f55d5a05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.f55d5a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7940a71626437234098",
+    "url": "/react-employee-directory/static/js/main.289a50cd.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
