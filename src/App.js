010b32a083958491af13a2eaf849a87272b30eb6
Connie Tran <connietrandev@gmail.com>
2020-07-16 10:06:00 -0400

    Add Material-UI. Add sort by first name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Container from '@material-ui/core/Container';
+
 import EmployeeCard from "./components/EmployeeCard";
-import employees from "./employees.json";
+import employeesArray from "./employees.json";
 
 function App() {
+  const [employees, setEmployees] = useState(employeesArray);
+
+  function sortFirstName() {
+    let sortEmployees = employees.sort((a, b) => {
+      let fa = a.first_name.toLowerCase(),
+        fb = b.first_name.toLowerCase();
+
+      if (fa < fb) {
+        return -1;
+      }
+      if (fa > fb) {
+        return 1;
+      }
+      return 0;
+    });
+
+    // setEmployees takes previous state value and changed state value
+    setEmployees(...employees, sortEmployees);
+  };
+
   return (
     <div className="wrapper">
-      <h1 className="title">Employee Directory</h1>
+      <Container maxWidth="sm">
+        <h1 className="title">Employee Directory</h1>
+      </Container>
+      <Container maxWidth="sm">
+        <ButtonGroup color="primary" aria-label="outlined primary button group">
+          <Button onClick={sortFirstName} >
+            Sort by First Name
+          </Button>
+          <Button>Sort by Last Name</Button>
+        </ButtonGroup>
+      </Container>
       {
-        employees.map(employee =>
-          <EmployeeCard 
-            firstName={employee.first_name} 
+        employeesArray.map(employee =>
+          <EmployeeCard
+            key={employee.id}
+            firstName={employee.first_name}
             lastName={employee.last_name}
-            title={employee.title} 
-            salary={employee.salary} 
-            dept={employee.department} 
-            manager={employee.manager} 
+            title={employee.title}
+            salary={employee.salary}
+            dept={employee.department}
+            manager={employee.manager}
           />)
       }
     </div>
